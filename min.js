26ee036cc151f3034a0a7195d74e7e97d7430554
Bbekraj <86979229+Bbekraj@users.noreply.github.com>
2022-06-08 14:04:47 +0545

    Add files via upload

---
+++ min.js
@@ -0,0 +1,15 @@
+<section class="top">
+<div class="container-fluid">
+    <ul>
+        <li><h3>Light<span>Up</span></h3></li>
+        <li><a>Home</a></li>
+        <li><a>Services</a></li>
+        <li><a>About</a></li>
+        <li><a>Portifolio</a></li>
+        <li><a>Team</a></li>
+        <li><a>Price</a></li>
+        <li><a>Contact</a></li>
+
+    </ul>
+</div>
+</section>
